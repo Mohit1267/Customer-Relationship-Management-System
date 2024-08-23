a5a449769e0a4443813e49ae3e9a4b26b06867eb
Mohit <mohitchandani4608@gmail.com>
2024-08-23 10:53:21 +0530

    Okay

--- stpa/settings.py
+++ stpa/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'stpa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'shanaya',
-#         'USER': 'root',
-#         'PASSWORD':'Mohit@sql1267',
-#         'HOST' : 'localhost',
-#         'PORT': '3306',
-#     }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',}
 
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shanaya',
+        'USER': 'root',
+        'PASSWORD':'Mohit@sql1267',
+        'HOST' : 'localhost',
+        'PORT': '3306',
+    }
+
+}
 
 
 # DATABASES = {
