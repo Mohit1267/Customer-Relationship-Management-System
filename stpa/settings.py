53003238b7a929ba2717bc26089536199379a111
“parn” <pjisvgreat@gmail.com>
2024-08-27 18:21:39 +0530

    some changes

--- stpa/settings.py
+++ stpa/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shanaya',
         'USER': 'root',
-        'PASSWORD':'mysql',
+        'PASSWORD':'Pj@123456',
         'HOST' : 'localhost',
         'PORT': '3306',
     }
